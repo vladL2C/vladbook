4ac13df9e2c32575b0b00bb0c95fdaf8ec03ed43
Vlad <Vlad@hotmail.co.nz>
2017-11-05 22:10:34 +1300

    added some changes to associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,16 @@ class User < ApplicationRecord
   has_one :avatar, class_name: "Photo", foreign_key: "user_id"  
 
   #Friendship associations
-  has_many :requesting_friends, -> { where :accepted => true }, class_name: "Friendship",
+  has_many :requesting_friends, class_name: "Friendship",
   															foreign_key: "request_id",
   															dependent: :destroy
+  has_many :accepted_friends, -> { where accepted: true }, class_name: "Friendship",
+  															foreign_key: "request_id",
+  															dependent: :destroy															
   has_many :requested_friends, class_name: "Friendship",
   															foreign_key: "requested_id",
   															dependent: :destroy
   has_many :friendship_requested, through: :requesting_friends, source: :requested
   has_many :friendship_requests, through: :requested_friends, source: :request	
-				
+	has_many :friends, through: :accepted_friends, source: :requested
 end
