28b572751843354797f679ea7285198ae787c06c
Vlad <Vlad@hotmail.co.nz>
2017-11-06 21:54:33 +1300

    adding friends works, need to optimize

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,8 @@
 class FriendshipsController < ApplicationController
+
+	def create
+		current_user.add_user(User.find(params[:requested_id]))
+		redirect_back(fallback_location: search_path)
+	end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get '/public_profile/:id', to: 'users#public_profile', as: :public_profile
   get '/search', to: 'users#find_user'
   resources :photos, only: [:create, :update] 
+  resources :friendships, only: [:create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
