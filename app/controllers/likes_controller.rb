8d3251774e0def0dc827d4d453794d389cad0d16
Vlad <Vlad@hotmail.co.nz>
2017-11-09 23:30:07 +1300

    like,comment,post working

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,23 @@
 class LikesController < ApplicationController
   
-  def create  
+  def create
+  	if @like = current_user.likes.new(user_id: params[:current_user], 
+  																 likable_id: params[:likable_id], 
+  																 likable_type: params[:likable_type])
+  	  @like.save
+  		redirect_to feed_path
+  	 else @like = current_user.likes.new(user_id: params[:current_user], 
+  																 likable_id: params[:likable_id], 
+  																 likable_type: params[:likable_type])
+  		@like.save
+  		redirect_to feed_path	
+  	end   
   end 
 
   def destroy
+  	@like = Like.find(params[:id])
+  	@like.destroy
+  	redirect_to feed_path
   end
    
 end
--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,2 +1,10 @@
 module LikesHelper
+
+	def liked_post?(post,user)
+		post.likes.pluck(:user_id).include?(user.id)
+	end 
+
+	def liked_comment?(comment,user)
+		comment.likes.pluck(:user_id).include?(user.id)
+	end 
 end
